402ad5f4d47b08b16016624ee52491d793a3ce7b
hafizhryadi <hafizhryadi@gmail.com>
2024-04-02 09:17:13 +0700

    add accessor image

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,11 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
+use function Pest\Laravel\get;
 
 class Post extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    protected function image(): Attribute {
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts/'.$image), 
+        );
+    }
 }
